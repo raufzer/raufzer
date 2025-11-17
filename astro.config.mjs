fc4f0a53d0a327f6d517fe4ee00e97e85adac478
Abd Raouf <a_zerkhef@estin.dz>
2025-11-17 15:14:35 +0100

    refactor: improve formatting and organization in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,28 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
+import rehypeMermaid from "rehype-mermaid";
+import remarkPlantUML from "remark-plantuml";
+
 export default defineConfig({
-  site: 'http://raufzer.me/',
+  site: "http://raufzer.me/",
   integrations: [react(), mdx()],
+
+  markdown: {
+    rehypePlugins: [
+      [rehypeMermaid, { strategy: "img-svg" }],
+    ],
+  },
+
   image: {
     remotePatterns: [],
     service: {
-      entrypoint: 'astro/assets/services/sharp'
-    }
+      entrypoint: "astro/assets/services/sharp",
+    },
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
